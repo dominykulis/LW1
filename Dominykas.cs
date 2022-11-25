f932c89014f74dc54773cb2a9c27d8e11adab970
Dominykas <dominykas.ceskevicius@stud.vilniustech.lt>
2022-11-25 13:40:14 +0200

    Code completely finished with added comments. Made the code easier to read and understand.

--- Dominykas.cs
+++ Dominykas.cs
@@ -7,22 +7,24 @@ internal class TextFieldParser
     {
         using(var reader = new StreamReader(@".\data.csv"))
         {
-            int i=0;
+            int amount_people=0;
             List<string> age = new List<string>();
-
+            
+            // reading file until the end storing the date value into a list and counting the number of dates
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
                 var values = line.Split(',');
                     
-                if (i>0)
+                if (amount_people>0)
                 {
                     age.Add(values[2]);
                 }
-                i++;
+                amount_people++;
             }
 
-            for(int f=1900; f < 2023; f++)
+            // testing each date from 1900 to 2023 to find matches of a certain year
+            for(int test_date=1900; test_date < 2023; test_date++)
             {
                 int matches = 0;
 
@@ -30,7 +32,7 @@ internal class TextFieldParser
                 {
                     int date_int = Convert.ToInt32(date);
 
-                    if(date_int == f)
+                    if(date_int == test_date)
                     {
                         matches++;
                     }
